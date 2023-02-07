e61823c5fc507d10fa597b3224d74d006dd39f8e
abuk111 <abuk111@op.pl>
2023-02-07 19:50:43 +0100

    add scrollspy and subpages cores

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@ const navbar = document.querySelector('.navbar')
 const navbarListItem = document.querySelectorAll('.navbar__listitem')
 const footerYear = document.querySelector('#date')
 
+const allSections = document.querySelectorAll('.section')
+const navItemHome = document.querySelector('.navitem-home')
+const navItemAboutus = document.querySelector('.navitem-aboutus')
+const navItemOffer = document.querySelector('.navitem-offer')
+const navItemContact = document.querySelector('.navitem-contact')
+
 const showNav = () => {
 	navbar.classList.add('navbar-active')
 	navbar.classList.remove('navbar-hide')
@@ -44,5 +50,49 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year
 }
 
+const activePageObserver = () => {
+	const currentPage = window.location.pathname
+
+	if (currentPage === '/contact.html') {
+		navItemContact.classList.add('nav-item-active')
+		navItemContact.children[0].classList.add('nav-item-active')
+	} else if (currentPage === '/offer.html') {
+		navItemOffer.classList.add('nav-item-active')
+		navItemOffer.children[0].classList.add('nav-item-active')
+	} else if (currentPage === '/index.html') {
+		navItemHome.classList.add('nav-item-active')
+		navItemHome.children[0].classList.add('nav-item-active')
+		window.addEventListener('scroll', activeSectionObserver)
+	}
+}
+
+const activeSectionObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('about-us') && section.offsetTop <= currentSection + 72) {
+			navSectionClear()
+			navItemAboutus.classList.add('nav-item-active')
+			navItemAboutus.children[0].classList.add('nav-item-active')
+		} else if (section.classList.contains('offer') && section.offsetTop <= currentSection + 72) {
+			navSectionClear()
+			navItemOffer.classList.add('nav-item-active')
+			navItemOffer.children[0].classList.add('nav-item-active')
+		} else if (section.classList.contains('header') && section.offsetTop <= currentSection + 72) {
+			navSectionClear()
+			navItemHome.classList.add('nav-item-active')
+			navItemHome.children[0].classList.add('nav-item-active')
+		}
+	})
+}
+
+const navSectionClear = () => {
+	navbarListItem.forEach(section => {
+		section.classList.remove('nav-item-active')
+		section.children[0].classList.remove('nav-item-active')
+	})
+}
+
 handleCurrentYear()
+activePageObserver()
 burgerBtn.addEventListener('click', navHandling)
